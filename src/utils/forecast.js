18d4040741cb870c872ea1aaa21b200af6acbada
Trent Davis <TDavis@csolsus.local>
2020-04-28 11:06:40 -0400

    Added humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude,longitude, callback) => {
       callback('Unable to find location. Try another search', undefined)
     }
     else {
-      callback(undefined, body.current.weather_descriptions[0] + ". It is currently "+ body.current.temperature + ". It feels like " + body.current.feelslike + " degrees out")
+      callback(undefined, body.current.weather_descriptions[0] + ". It is currently "+ body.current.temperature + ". It feels like " + body.current.feelslike + " out. The humidity is " + body.current.humidity+ "%.")
     }
   })
 }
